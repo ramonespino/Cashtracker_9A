6e12420422d5eebbc6c1d2a27275d93331a2ad0a
LuisEmpanadas <luisvaldezjmz@gmail.com>
2025-11-03 12:56:40 -0600

    hola fakers

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,19 @@
 import express from "express";
 import colors from "colors";
 import morgan from "morgan";
+import { db } from "./config/db";
+
+async function connectDB() {
+  try {
+    await db.authenticate();
+    db.sync();
+    console.log(colors.blue.bold("Conexion exitosa a la BD"));
+  } catch (error) {
+    //console.log(error)
+    console.log(colors.red.bold("Fallo la conexion a la BD"));
+  }
+}
+connectDB();
 
 const app = express();
 
